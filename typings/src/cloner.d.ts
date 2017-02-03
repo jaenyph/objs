a4525a26c77cc21fd9c92113636ecb7b8e4c8ba0
jaenyph <jaenyph@pelemail.net>
2017-02-03 13:58:13 +0100

    Discard typings generation on build as it conflicts with watcher

--- typings/src/cloner.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="types.d.ts" />
-declare namespace Objs {
-    class Cloner {
-        static shallowClone<T>(value: T): T;
-        private static cloneNonArray(value, deepCloning);
-        static deepClone<T>(value: T): T;
-    }
-}
