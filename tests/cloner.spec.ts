359b346f5753544679c88d3f912f73e20a4b42c9
jaenyph <jaenyph@pelemail.net>
2017-02-03 13:58:28 +0100

    Add cloner tests

--- tests/cloner.spec.ts
+++ tests/cloner.spec.ts
@@ -14,6 +14,36 @@ describe("Objs.Cloner", () => {
         expect(actual[0]).not.toBe(originalArray[0]);
     });
 
+    it("shallowClone returns new date", () => {
+        const originalObject = new Date();
+        const actual = sut.shallowClone(originalObject);
+        expect(actual).not.toBe(originalObject);
+    });
+
+    it("shallowClone clone date", () => {
+        const originalObject = new Date();
+        const actual = sut.shallowClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
+    it("shallowClone clone string", () => {
+        const originalObject = "";
+        const actual = sut.shallowClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
+    it("shallowClone clone boolean", () => {
+        const originalObject = true;
+        const actual = sut.shallowClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
+    it("shallowClone clone number", () => {
+        const originalObject = 3.14;
+        const actual = sut.shallowClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
     it("shallowClone returns new object", () => {
         const originalObject = {};
         const actual = sut.shallowClone(originalObject);
@@ -29,6 +59,17 @@ describe("Objs.Cloner", () => {
         expect(actual.shared).toBe(shared);
     });
 
+    it("shallowClone does not clone object nested properties", () => {
+        const shared = {};
+        const originalObject = {
+            "nested" : {
+                "shared" : shared
+            }
+        };
+        const actual = sut.shallowClone(originalObject);
+        expect(actual.nested.shared).toBe(shared);
+    });
+
     it("deepClone returns new array", () => {
         const originalArray: any[] = [];
         const actual = sut.deepClone(originalArray);
@@ -41,9 +82,99 @@ describe("Objs.Cloner", () => {
         expect(actual[0]).not.toBe(originalArray[0]);
     });
 
+    it("deepClone returns new date", () => {
+        const originalObject = new Date();
+        const actual = sut.deepClone(originalObject);
+        expect(actual).not.toBe(originalObject);
+    });
+
+    it("deepClone clone date", () => {
+        const originalObject = new Date();
+        const actual = sut.deepClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
+    it("deepClone clone string", () => {
+        const originalObject = "";
+        const actual = sut.deepClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
+    it("deepClone clone boolean", () => {
+        const originalObject = true;
+        const actual = sut.deepClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
+    it("deepClone clone number", () => {
+        const originalObject = 3.14;
+        const actual = sut.deepClone(originalObject);
+        expect(actual).toEqual(originalObject);
+    });
+
     it("deepClone returns new object", () => {
         const originalObject = {};
         const actual = sut.deepClone(originalObject);
         expect(actual).not.toBe(originalObject);
+    });
+
+    it("deepClone clones object properties", () => {
+        const notShared = {};
+        const originalObject = {
+            "notShared" : notShared
+        };
+        const actual = sut.deepClone(originalObject);
+        expect(actual.notShared).not.toBe(notShared);
+    });
+
+    it("deepClone clones object array properties", () => {
+        const notShared = [{}];
+        const originalObject = {
+            "notShared" : notShared
+        };
+        const actual = sut.deepClone(originalObject);
+        expect(actual.notShared).not.toBe(notShared);
+    });
+
+    it("deepClone clones object array properties elements", () => {
+        const notShared = [{}];
+        const originalObject = {
+            "notShared" : notShared
+        };
+        const actual = sut.deepClone(originalObject);
+        expect(actual.notShared[0]).not.toBe(notShared[0]);
+    });
+
+    it("deepClone clones nested object properties", () => {
+        const notShared = {};
+        const originalObject = {
+            "nested" : {
+                "notShared" : notShared
+            }
+        };
+        const actual = sut.deepClone(originalObject);
+        expect(actual.nested.notShared).not.toBe(notShared);
+    });
+
+    it("deepClone clones nested object array properties", () => {
+        const notShared = [{}];
+        const originalObject = {
+            "nested" : {
+                "notShared" : notShared
+            }
+        };
+        const actual = sut.deepClone(originalObject);
+        expect(actual.nested.notShared).not.toBe(notShared);
+    });
+
+    it("deepClone clones nested object array properties elements", () => {
+        const notShared = [{}];
+        const originalObject = {
+            "nested" : {
+                "notShared" : notShared
+            }
+        };
+        const actual = sut.deepClone(originalObject);
+        expect(actual.nested.notShared[0]).not.toBe(notShared[0]);
     });
 });
