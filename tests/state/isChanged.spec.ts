bd31de3dabeec548155c07cfc9bf1572d940db48
jaenyph <jaenyph@pelemail.net>
2017-02-05 20:02:45 +0100

    Add some more unit tests for State

--- tests/state/isChanged.spec.ts
+++ tests/state/isChanged.spec.ts
@@ -1,35 +1,131 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 /// <reference path="../../src/state.ts" />
 describe("Objs.State.isChanged", () => {
-    const sut = new Objs.State();
+    const notDefinedErrorMessage = "value is not defined";
+    const primitiveErrorMessage = "could not act on a primitive value";
+    const missingIdErrorMessage = "value does not defined an 'id' key"
+    let sutConfiguration: Objs.IStateConfiguration;
 
-    const primitiveErrorMessage: string = "could not act on a primitive value";
+    const getSut = () => {
+        if (sutConfiguration === undefined) {
+            throw new Error("sut configuration not set");
+        }
+        return new Objs.State(sutConfiguration);
+    };
 
-    it("throws when trying to check a boolean", () => {
+    const withReferenceTracking = () => {
+        sutConfiguration = {
+            historyDepth: 1,
+            pristineKind: Objs.PristineKind.DeepClone,
+            propertyNameCasingKind: Objs.PropertyNameCasingKind.LowerCamelCase,
+            trackingKind: Objs.TrackingKind.Reference
+        };
+    }
+
+    const withIdTracking = () => {
+        sutConfiguration = {
+            historyDepth: 1,
+            pristineKind: Objs.PristineKind.DeepClone,
+            propertyNameCasingKind: Objs.PropertyNameCasingKind.LowerCamelCase,
+            trackingKind: Objs.TrackingKind.Id
+        };
+    }
+
+    beforeEach(() => {
+        sutConfiguration = undefined as any as Objs.IStateConfiguration;
+    });
+
+    it("throws when trying to check null in reference tracking mode", () => {
+        withReferenceTracking();
+        const valueToSave = null;
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to check null in id tracking mode", () => {
+        withIdTracking();
+        const valueToSave = null;
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to check undefined in reference tracking mode", () => {
+        withReferenceTracking();
+        const valueToSave = undefined;
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to check undefined in id tracking mode", () => {
+        withIdTracking();
+        const valueToSave = undefined;
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to check a boolean in reference mode", () => {
+        withReferenceTracking();
         const valueToCheck = true;
+        const sut = getSut();
         expect(sut.isChanged.bind(sut, valueToCheck)).toThrowError(primitiveErrorMessage);
     });
 
-    it("throws when trying to check a number", () => {
+    it("throws when trying to check a boolean in id mode", () => {
+        withIdTracking();
+        const valueToCheck = true;
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToCheck)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("throws when trying to check a number in reference mode", () => {
+        withReferenceTracking();
         const valueToCheck = 3.14;
+        const sut = getSut();
         expect(sut.reset.bind(sut, valueToCheck)).toThrowError(primitiveErrorMessage);
     });
 
-    it("throws when trying to check a string", () => {
+    it("throws when trying to check a number in id mode", () => {
+        withIdTracking();
+        const valueToCheck = 3.14;
+        const sut = getSut();
+        expect(sut.reset.bind(sut, valueToCheck)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("throws when trying to check a string in reference mode", () => {
+        withReferenceTracking();
         const valueToCheck = "abc";
+        const sut = getSut();
         expect(sut.isChanged.bind(sut, valueToCheck)).toThrowError(primitiveErrorMessage);
     });
 
-    it("does not throws when trying to check a date", () => {
-        const valueToCheck = new Date()
+    it("throws when trying to check a string in id mode", () => {
+        withIdTracking();
+        const valueToCheck = "abc";
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToCheck)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("does not throws when trying to check a date in reference mode", () => {
+        withReferenceTracking();
+        const valueToCheck = new Date();
+        const sut = getSut();
         expect(sut.isChanged.bind(sut, valueToCheck)).not.toThrowError(primitiveErrorMessage);
     });
 
-    it("returns false if tracked object properties have not changed", () => {
+    it("throws when trying to check a date in id mode", () => {
+        withIdTracking();
+        const valueToCheck = new Date();
+        const sut = getSut();
+        expect(sut.isChanged.bind(sut, valueToCheck)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("returns false if tracked object properties have not changed in reference mode", () => {
         // arrange
+        withReferenceTracking();
         const trackedObject = {
             "prop": "old"
         };
+        const sut = getSut();
         sut.save(trackedObject);
         trackedObject.prop = "old";
 
@@ -40,11 +136,13 @@ describe("Objs.State.isChanged", () => {
         expect(actual).not.toBe(true);
     });
 
-    it("returns true if tracked object properties have changed", () => {
+    it("returns true if tracked object properties have changed in reference mode", () => {
         // arrange
+        withReferenceTracking();
         const trackedObject = {
             "prop": "old"
         };
+        const sut = getSut();
         sut.save(trackedObject);
         trackedObject.prop = "new";
 
@@ -55,11 +153,13 @@ describe("Objs.State.isChanged", () => {
         expect(actual).toBe(true);
     });
 
-    it("returns true if tracked object array properties elements have changed", () => {
+    it("returns true if tracked object array properties elements have changed in reference mode", () => {
         // arrange
+        withReferenceTracking();
         const trackedObject = {
             "prop": [3.14]
         };
+        const sut = getSut();
         sut.save(trackedObject);
         trackedObject.prop[0] = 1.59;
 
@@ -70,11 +170,13 @@ describe("Objs.State.isChanged", () => {
         expect(actual).toBe(true);
     });
 
-    it("returns true if tracked object nested array elements have changed", () => {
+    it("returns true if tracked object nested array elements have changed in reference mode", () => {
         // arrange
+        withReferenceTracking();
         const trackedObject = {
             "nested": [3.14]
         };
+        const sut = getSut();
         sut.save(trackedObject);
         trackedObject.nested[0] = 1.59;
 
@@ -87,9 +189,11 @@ describe("Objs.State.isChanged", () => {
 
     it("returns true if tracked object nested array elements properties have changed", () => {
         // arrange
+        withReferenceTracking();
         const trackedObject = {
             "nested": [{ "prop": 3.14 }]
         };
+        const sut = getSut();
         sut.save(trackedObject);
         trackedObject.nested[0].prop = 1.59;
 
--- tests/state/revert.spec.ts
+++ tests/state/revert.spec.ts
@@ -1,35 +1,131 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 /// <reference path="../../src/state.ts" />
 describe("Objs.State.revert", () => {
-    const sut = new Objs.State();
+    const notDefinedErrorMessage = "value is not defined";
+    const primitiveErrorMessage = "could not act on a primitive value";
+    const missingIdErrorMessage = "value does not defined an 'id' key"
+    let sutConfiguration: Objs.IStateConfiguration;
 
-    const primitiveErrorMessage: string = "could not act on a primitive value";
+    const getSut = () => {
+        if (sutConfiguration === undefined) {
+            throw new Error("sut configuration not set");
+        }
+        return new Objs.State(sutConfiguration);
+    };
 
-    it("throws when trying to revert a boolean", () => {
+    const withReferenceTracking = () => {
+        sutConfiguration = {
+            historyDepth: 1,
+            pristineKind: Objs.PristineKind.DeepClone,
+            propertyNameCasingKind: Objs.PropertyNameCasingKind.LowerCamelCase,
+            trackingKind: Objs.TrackingKind.Reference
+        };
+    }
+
+    const withIdTracking = () => {
+        sutConfiguration = {
+            historyDepth: 1,
+            pristineKind: Objs.PristineKind.DeepClone,
+            propertyNameCasingKind: Objs.PropertyNameCasingKind.LowerCamelCase,
+            trackingKind: Objs.TrackingKind.Id
+        };
+    }
+
+    beforeEach(() => {
+        sutConfiguration = undefined as any as Objs.IStateConfiguration;
+    });
+
+    it("throws when trying to revert null in reference tracking mode", () => {
+        withReferenceTracking();
+        const valueToSave = null;
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to revert null in id tracking mode", () => {
+        withIdTracking();
+        const valueToSave = null;
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to revert undefined in reference tracking mode", () => {
+        withReferenceTracking();
+        const valueToSave = undefined;
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to revert undefined in id tracking mode", () => {
+        withIdTracking();
+        const valueToSave = undefined;
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToSave)).toThrowError(notDefinedErrorMessage);
+    });
+
+    it("throws when trying to revert a boolean in reference tracking mode", () => {
+        withReferenceTracking();
         const valueToRevert = true;
+        const sut = getSut();
         expect(sut.revert.bind(sut, valueToRevert)).toThrowError(primitiveErrorMessage);
     });
 
-    it("throws when trying to revert a number", () => {
+    it("throws when trying to revert a boolean in id tracking mode", () => {
+        withIdTracking();
+        const valueToRevert = true;
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToRevert)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("throws when trying to revert a number in reference tracking mode", () => {
+        withReferenceTracking();
         const valueToRevert = 3.14;
+        const sut = getSut();
         expect(sut.revert.bind(sut, valueToRevert)).toThrowError(primitiveErrorMessage);
     });
 
-    it("throws when trying to revert a string", () => {
+    it("throws when trying to revert a number in id tracking mode", () => {
+        withIdTracking();
+        const valueToRevert = 3.14;
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToRevert)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("throws when trying to revert a string in reference tracking mode", () => {
+        withReferenceTracking();
         const valueToRevert = "abc";
+        const sut = getSut();
         expect(sut.revert.bind(sut, valueToRevert)).toThrowError(primitiveErrorMessage);
     });
 
-    it("does not throws when trying to revert a date", () => {
+    it("throws when trying to revert a string in id tracking mode", () => {
+        withIdTracking();
+        const valueToRevert = "abc";
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToRevert)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("does not throws when trying to revert a date in reference tracking mode", () => {
+        withReferenceTracking();
         const valueToRevert = new Date();
+        const sut = getSut();
         expect(sut.revert.bind(sut, valueToRevert)).not.toThrowError(primitiveErrorMessage);
     });
 
-    it("is reverted to previous state", () => {
+    it("throws when trying to revert a date in id tracking mode", () => {
+        withIdTracking();
+        const valueToRevert = new Date();
+        const sut = getSut();
+        expect(sut.revert.bind(sut, valueToRevert)).toThrowError(missingIdErrorMessage);
+    });
+
+    it("is reverted to previous state in reference tracking mode", () => {
         // arrange
+        withReferenceTracking();
         const tracked = {
-            "prop" : "old"
+            "prop": "old"
         };
+        const sut = getSut();
         sut.save(tracked);
         tracked.prop = "new";
 
@@ -38,5 +134,40 @@ describe("Objs.State.revert", () => {
 
         // assert
         expect(actual).not.toEqual(tracked);
+    });
+
+    it("is reverted to previous state in id tracking mode with same reference", () => {
+        // arrange
+        withIdTracking();
+        const tracked = {
+            "id": 1,
+            "prop": "old"
+        };
+        const sut = getSut();
+        sut.save(tracked);
+        tracked.prop = "new";
+
+        // act
+        const actual = sut.revert(tracked);
+
+        // assert
+        expect(actual).not.toEqual(tracked);
+    });
+
+    it("is reverted to previous state in id tracking mode with another reference", () => {
+        // arrange
+        withIdTracking();
+        const tracked = {
+            "id": 1,
+            "prop": "old"
+        };
+        const sut = getSut();
+        sut.save(tracked);
+
+        // act
+        const actual = sut.revert({ "id": 1 });
+
+        // assert
+        expect(actual).toEqual(tracked);
     });
 });
